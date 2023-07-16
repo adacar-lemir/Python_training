b296703012011ff5bbe31189ce5a1e7c471587f4
Nicolás Fleitas Caputi <adacar.lemir@gmail.com>
2023-07-16 16:16:23 -0300

    added users list and getUser and GetUsers

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -14,16 +14,27 @@ class User(BaseModel):
     createdDate:datetime = datetime.now()
 
 app = FastAPI()
+users = []
 
-@app.get("/welcome")
+@app.get('/welcome')
 def welcome():
     return("Welcome")
 
-@app.post("/withParam")
-def withParam(user:User):
+@app.get('/user')
+def getUsers():
+    return users
+
+@app.post('/user')
+def createUser(user:User):
     user = user.dict()
-    print(user)
-    return True
+    users.append(user)
+    return ("user created succesfully")
+
+@app.get('/user/{user_id}')
+def getUser(user_id:int):
+    for user in users:
+        if user['id']==user_id:
+            return user
 
 if __name__=="__main__":
     uvicorn.run("main:app", port=8000, reload=True)
